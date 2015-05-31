25bd27c5cbf971a677fdc975abea6f488584d241
Puggyblue999 <Puggyblue999@users.noreply.github.com>
2015-05-31 12:54:47 -0400

    DNA files are now in a separate folder

---
+++ run-.bat
@@ -0,0 +1,8 @@
+set PATH=lib
+set PYTHONPATH=.;lib-tk
+set TTMODELS=../resources
+set DMODELS=/i/alpha/player/install/dmodels
+set CFG_PATH=.
+set CONFIG_CONFIG=:configpath=CFG_PATH
+C:\Panda3D-1.9.0-TTR\python\ppython.exe LevelEditorStart.py TT
+
